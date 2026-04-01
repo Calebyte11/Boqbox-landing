0d1b1e9f42f2680bf46b76020d13646e2c5824cd
Calebyte11 <calebytedembelle11@gmail.com>
2026-04-01 12:16:43 +0100

    Implementation of Search Items Version III (for development build II)

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
 import Nav from '../components/Nav';
 import StepIndicator from '../components/StepIndicator';
